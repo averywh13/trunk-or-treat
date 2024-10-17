31f6df7363635aa38bac3c5077d15ca344388a7f
averywh13 <awekslerhart@gmail.com>
2024-10-16 21:01:59 -0400

    chapter 6 - completely finished

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
+$(document).ready(function () {
+    $("#toggleDesc").on("click", function () {
+        $("#eventDescription").toggle();
+    });
+    
+    $(".event-heading").on("click", function () {
+        $(this).next(".event-details").slideToggle();
+    });
+    
+    $(".trunk-img").on("mouseenter", function () {
+        $(this).next(".trunk-description").fadeIn();
+    }).on("mouseleave", function () {
+        $(this).next(".trunk-description").fadeOut();
+    });
 
+    $(".trunk-img").on("mouseenter", function () {
+        $(this).addClass("zoomed"); // Add the zoomed class on mouse enter
+    }).on("mouseleave", function () {
+        $(this).removeClass("zoomed"); // Remove the zoomed class on mouse leave
+    });
+
+
+
+
+
+
+
+
+});
 
 
 
